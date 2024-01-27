535d03a3a7df13fe5de209664578a0f63b43f011
Akhil Vanka <dev+github@akhilv.me>
2024-01-27 14:12:22 -0500

    feat(core)!: added persistence for the lifecycle of the user's session

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase/firebase";
-import { NavLink, useNavigate } from "react-router-dom";
+import auth from "../firebase/firebase";
+import { useNavigate } from "react-router-dom";
 import SignIn from "../components/google-sign-in";
 
 const Login = () => {
