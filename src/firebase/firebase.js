6f25aa78b164561c9b32395fefc38981747b9ee0
Akhil Vanka <dev+github@akhilv.me>
2024-01-28 09:44:54 -0500

    fix(core)!: switch to env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithPopup, GoogleAuthProvider, setPersistence, browserSessionPersistence, browserLocalPersistence } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, setPersistence, browserSessionPersistence, browserLocalPersistence, applyActionCode } from "firebase/auth";
 // import { }
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,14 +9,23 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCVdj4crJC7izx-OJ7JVYs8p9WYuDVviMM",
+//   authDomain: "detectorinjector.firebaseapp.com",
+//   projectId: "detectorinjector",
+//   storageBucket: "detectorinjector.appspot.com",
+//   messagingSenderId: "26607904664",
+//   appId: "1:26607904664:web:982d77bf712614901e6ded",
+//   measurementId: "G-SERTEJY9XZ"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCVdj4crJC7izx-OJ7JVYs8p9WYuDVviMM",
-  authDomain: "detectorinjector.firebaseapp.com",
-  projectId: "detectorinjector",
-  storageBucket: "detectorinjector.appspot.com",
-  messagingSenderId: "26607904664",
-  appId: "1:26607904664:web:982d77bf712614901e6ded",
-  measurementId: "G-SERTEJY9XZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
