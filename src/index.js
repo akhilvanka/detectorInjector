b0d2971d8943afd1bd491d3713fb2937fea402a1
Theo <theomhalpern@gmail.com>
2025-02-11 11:42:03 -0500

    added posthog

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 
+import { PostHogProvider} from 'posthog-js/react'
+
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <PostHogProvider
+            apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+            options={options}
+        >
+            <App />
+        </PostHogProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
